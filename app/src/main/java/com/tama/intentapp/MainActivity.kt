38409b78e06b556fe77b0e871b269b127458703a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-06 11:40:12 +0700

    move to other activity using explicit intent

--- app/src/main/java/com/tama/intentapp/MainActivity.kt
+++ app/src/main/java/com/tama/intentapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tama.intentapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -21,7 +22,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_move_activity -> {
-
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }
--- app/src/main/java/com/tama/intentapp/MoveActivity.kt
+++ app/src/main/java/com/tama/intentapp/MoveActivity.kt
@@ -11,10 +11,6 @@ class MoveActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_move)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
