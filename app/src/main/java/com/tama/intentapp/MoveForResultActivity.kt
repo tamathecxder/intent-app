f000f01f6b734060fa5029144f8644e039650d0a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-07 23:51:03 +0700

    init MoveForResultActivity

---
+++ app/src/main/java/com/tama/intentapp/MoveForResultActivity.kt
@@ -0,0 +1,20 @@
+package com.tama.intentapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveForResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move_for_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
