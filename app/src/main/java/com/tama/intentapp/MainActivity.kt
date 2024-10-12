aa3e4cfd41eb572b8df04aad4ff058b042b2fb7c
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-12 10:46:40 +0700

    impl getting a result from other activity

--- app/src/main/java/com/tama/intentapp/MainActivity.kt
+++ app/src/main/java/com/tama/intentapp/MainActivity.kt
@@ -5,12 +5,26 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvResult: TextView;
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        result ->
+            if (result.resultCode == MoveForResultActivity.RESULT_CODE && result.data != null) {
+                val selectedValue = result.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
+                tvResult.text = "Result: $selectedValue"
+            }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +41,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnDialPhone:Button = findViewById(R.id.btn_dial_number)
         btnDialPhone.setOnClickListener(this)
+
+        val btnMoveForResult:Button = findViewById(R.id.btn_move_for_result)
+        btnMoveForResult.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
     }
 
     override fun onClick(v: View?) {
@@ -60,6 +79,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val phoneNumber = "081210841382"
                 val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
                 startActivity(dialPhoneIntent)
+            }
+
+            R.id.btn_move_for_result -> {
+                val moveForResultIntent = Intent(this@MainActivity, MoveForResultActivity::class.java)
+                resultLauncher.launch(moveForResultIntent)
             }
         }
     }
--- app/src/main/java/com/tama/intentapp/MoveForResultActivity.kt
+++ app/src/main/java/com/tama/intentapp/MoveForResultActivity.kt
@@ -1,20 +1,55 @@
 package com.tama.intentapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MoveForResultActivity : AppCompatActivity() {
+class MoveForResultActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+        const val RESULT_CODE = 110
+    }
+
+    private lateinit var btnChoose: Button
+    private lateinit var rgNumber: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_move_for_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnChoose = findViewById(R.id.btn_choose)
+        rgNumber = findViewById(R.id.rg_number)
+
+        btnChoose.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_choose) {
+            if (rgNumber.checkedRadioButtonId > 0) {
+                var value = 0
+
+                when (rgNumber.checkedRadioButtonId) {
+                    R.id.rb_50 -> value = 50
+
+                    R.id.rb_100 -> value = 100
+
+                    R.id.rb_150 -> value = 150
+
+                    R.id.rb_200 -> value = 200
+                }
+
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_SELECTED_VALUE, value)
+                setResult(RESULT_CODE, resultIntent)
+                finish()
+            }
         }
     }
 }
